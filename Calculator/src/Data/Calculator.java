b9203518d83f4353cdbd25ec4ce765fae446b3df
Camilo Vasquez <camilovasquez9738@hotmail.com>
2021-01-21 16:20:35 -0500

    substract and sum methods added

--- Calculator/src/Data/Calculator.java
+++ Calculator/src/Data/Calculator.java
@@ -2,12 +2,22 @@ package Data;
 
 public class Calculator {
 
+
     public int multiplyMethod(int num1, int num2) {
         return num1 * num2;
     }
 
     public int divitionMethod(int num1, int num2) {
         return num1 / num2;
+    }
+
+
+    public int sumMethod(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int substractMethod(int num1, int num2) {
+        return num1 - num2;
     }
 
 }
