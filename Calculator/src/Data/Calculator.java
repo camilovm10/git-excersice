e1083858b92ca3ec2b1847fe6bab7cdfdfdd9d8e
Camilo Vasquez <camilovasquez9738@hotmail.com>
2021-01-21 16:23:28 -0500

    multiply and divition methods added

---
+++ Calculator/src/Data/Calculator.java
@@ -0,0 +1,13 @@
+package Data;
+
+public class Calculator {
+
+    public int multiplyMethod(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int divitionMethod(int num1, int num2) {
+        return num1 / num2;
+    }
+
+}
